15aed58912f563e4b79a0a91865205a8f5d837e0
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-08 20:38:07 +0200

    Add navigation buttons to pages list

--- src/components/PagesList.tsx
+++ src/components/PagesList.tsx
@@ -3,22 +3,34 @@ import { useVideosContext } from "../context/VideosContext";
 
 function PagesList() {
   const { pagesCount, setCurrentPage, currentPage } = useVideosContext();
+  const setPage = (page: number) => {
+    if (currentPage !== page) {
+      setCurrentPage(page);
+      window.scrollTo(0, 0);
+    }
+  };
+
   return (
     <Pagination aria-label="Videos list">
+      <PaginationItem>
+        <PaginationLink first onClick={() => setPage(0)} />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink previous onClick={() => setPage(currentPage - 1)} />
+      </PaginationItem>
       {[...Array(pagesCount).keys()].map((page) => (
         <PaginationItem key={`page-${page}`} active={currentPage === page}>
-          <PaginationLink
-            onClick={() => {
-              if (currentPage !== page) {
-                setCurrentPage(page);
-                window.scrollTo(0, 0);
-              }
-            }}
-          >
+          <PaginationLink onClick={() => setPage(page)}>
             {page + 1}
           </PaginationLink>
         </PaginationItem>
       ))}
+      <PaginationItem>
+        <PaginationLink next onClick={() => setPage(currentPage + 1)} />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink last onClick={() => setPage(pagesCount - 1)} />
+      </PaginationItem>
     </Pagination>
   );
 }
