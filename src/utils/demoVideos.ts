c5b9f5947856ebbe7f2a09c63e9b119b4aa0d73a
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-08 17:12:33 +0200

    Fix timestamp handling in demo data

--- src/utils/demoVideos.ts
+++ src/utils/demoVideos.ts
@@ -1,12 +1,14 @@
+const timestamp = Date.now();
+
 const demoVideos = [
-  { id: "580725557", favorite: true, timestamp: Date.now() },
-  { id: "cTQ3Ko9ZKg8", favorite: false, timestamp: Date.now() },
-  { id: "um2Q9aUecy0", favorite: false, timestamp: Date.now() },
-  { id: "3bSZk2Xne8o", favorite: false, timestamp: Date.now() },
-  { id: "aGGBGcjdjXA", favorite: false, timestamp: Date.now() },
-  { id: "GfO-3Oir-qM", favorite: false, timestamp: Date.now() },
-  { id: "JkaxUblCGz0", favorite: true, timestamp: Date.now() },
-  { id: "XmtXC_n6X6Q", favorite: false, timestamp: Date.now() },
+  { id: "580725557", favorite: false, timestamp },
+  { id: "cTQ3Ko9ZKg8", favorite: false, timestamp },
+  { id: "um2Q9aUecy0", favorite: false, timestamp },
+  { id: "3bSZk2Xne8o", favorite: false, timestamp },
+  { id: "aGGBGcjdjXA", favorite: true, timestamp },
+  { id: "GfO-3Oir-qM", favorite: false, timestamp },
+  { id: "JkaxUblCGz0", favorite: true, timestamp },
+  { id: "XmtXC_n6X6Q", favorite: false, timestamp },
 ];
 
 export default demoVideos;
