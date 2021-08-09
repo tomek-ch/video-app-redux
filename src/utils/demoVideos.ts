c16e49155549c0371c4ca7d602e0acaf67630271
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-09 23:12:18 +0200

    Change order of demo videos

--- src/utils/demoVideos.ts
+++ src/utils/demoVideos.ts
@@ -1,7 +1,6 @@
 const timestamp = Date.now();
 
 const demoVideos = [
-  { id: "580725557", favorite: false, timestamp },
   { id: "cTQ3Ko9ZKg8", favorite: false, timestamp },
   { id: "um2Q9aUecy0", favorite: false, timestamp },
   { id: "3bSZk2Xne8o", favorite: false, timestamp },
@@ -9,6 +8,7 @@ const demoVideos = [
   { id: "GfO-3Oir-qM", favorite: false, timestamp },
   { id: "JkaxUblCGz0", favorite: true, timestamp },
   { id: "XmtXC_n6X6Q", favorite: false, timestamp },
+  { id: "580725557", favorite: false, timestamp },
 ];
 
 export default demoVideos;
